83042f7d9dabc0b02217da74569946b073378272
jimacsx <maximino.peralta5791@gmail.com>
2022-12-12 11:06:11 -0600

    Primera versión del módulo de operaciones básicas de matemáticas

---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Suma de dos números
+   * @example
+   * n1 = 1, n2 = 3 => resultado 4
+   * @param {*} n1 número uno de la suma
+   * @param {*} n2 número dos de la suma
+   * @returns
+   */
+  suma: function (n1, n2) {
+    return n1 + n2;
+  },
+  /**
+   * Resta de dos números
+   * @example
+   * n1 = 1, n2 = 3 => resultado -2
+   * @param {*} n1 número uno de la resta
+   * @param {*} n2 número dos de la resta
+   * @returns
+   */
+  resta: function (n1, n2) {
+    return n1 - n2;
+  },
+  /**
+   * Multiplicación de dos números
+   * @example
+   * n1 = 1, n2 = 3 => resultado 3
+   * @param {*} n1 número uno de la multiplicación
+   * @param {*} n2 número dos de la multiplicación
+   * @returns
+   */
+  multiplicacion: function (n1, n2) {
+    return n1 * n2;
+  },
+  /**
+   * División de dos números
+   * @example
+   * n1 = 1, n2 = 3 => resultado 0.33
+   * @param {*} n1 número uno de la división
+   * @param {*} n2 número dos de la división
+   * @returns
+   */
+  division: function (n1, n2) {
+    return n1 / n2;
+  },
+};
