adfdcdd00df94b52dc7f58d4e3c1c34a1546dc41
jimacsx <maximino.peralta5791@gmail.com>
2022-12-12 14:53:18 -0600

    Funcionalidad para validar números OK

--- index.js
+++ index.js
@@ -2,45 +2,61 @@ module.exports = {
   /**
    * Suma de dos números
    * @example
-   * n1 = 1, n2 = 3 => resultado 4
-   * @param {*} n1 número uno de la suma
-   * @param {*} n2 número dos de la suma
-   * @returns
+   * 1 + 3 = 4
+   * @param { number } n1 Número uno de la suma
+   * @param { number } n2 Número dos de la suma
+   * @returns n1 + n2
    */
   suma: function (n1, n2) {
-    return n1 + n2;
+    return this.esNumero(n1, n2) ? n1 + n2 : this.mensajeError();
   },
   /**
    * Resta de dos números
    * @example
-   * n1 = 1, n2 = 3 => resultado -2
-   * @param {*} n1 número uno de la resta
-   * @param {*} n2 número dos de la resta
-   * @returns
+   * 1 - 3 = -2
+   * @param { number } n1 Número uno de la resta
+   * @param { number } n2 Número dos de la resta
+   * @returns n1 - n2
    */
   resta: function (n1, n2) {
-    return n1 - n2;
+    return this.esNumero(n1, n2) ? n1 - n2 : this.mensajeError();
   },
   /**
    * Multiplicación de dos números
    * @example
-   * n1 = 1, n2 = 3 => resultado 3
-   * @param {*} n1 número uno de la multiplicación
-   * @param {*} n2 número dos de la multiplicación
-   * @returns
+   * 1 * 3 = 3
+   * @param { number } n1 Número uno de la multiplicación
+   * @param { number } n2 Número dos de la multiplicación
+   * @returns n1 * n2
    */
   multiplicacion: function (n1, n2) {
-    return n1 * n2;
+    return this.esNumero(n1, n2) ? n1 * n2 : this.mensajeError();
   },
   /**
    * División de dos números
    * @example
-   * n1 = 1, n2 = 3 => resultado 0.33
-   * @param {*} n1 número uno de la división
-   * @param {*} n2 número dos de la división
-   * @returns
+   * 1 / 1 = 1
+   * @param { number } n1 Número uno de la división
+   * @param { number } n2 Número dos de la división
+   * @returns n1 / n2
    */
   division: function (n1, n2) {
-    return n1 / n2;
+    return this.esNumero(n1, n2) ? n1 / n2 : this.mensajeError();
+  },
+  /**
+   * Comprueba si los valores introducidos son de tipo número
+   * @param { number } n1
+   * @param { number } n2
+   */
+  esNumero: function (n1, n2) {
+    return typeof n1 === "number" && typeof n2 === "number";
+  },
+  /**
+   * Muestra un mensaje de error cuando no es número uno de los dos o los dos
+   */
+  mensajeError: function () {
+    console.log(
+      "Uno de los valores introducidos o los dos no son de tipo numérico"
+    );
   },
 };
--- pruebas.js
+++ pruebas.js
@@ -1,6 +1,10 @@
 const m = require(".");
 
 console.log(m.suma(1, 2));
+console.log(m.suma(1, "hola mundo"));
 console.log(m.resta(11, 24));
+console.log(m.resta(true, 24));
 console.log(m.multiplicacion(1, 12));
-console.log(m.division(1, 24));
+console.log(m.multiplicacion("hola", "mundo"));
+console.log(m.division(1, 4));
+console.log(m.division(true, false));
